66e51053d6f883da5dc7dc467cdcc77a61bdef78
jakewhite1230 <jwhite@learn4life.org>
2016-08-16 13:22:44 -0700

    Fixed Buttons
    Fixed the download, twitter, and facebook buttons

--- inc/alabaster-sermonmanager-audio-skin.php
+++ inc/alabaster-sermonmanager-audio-skin.php
@@ -16,7 +16,7 @@ if($style_options != '' ){
  $sermonSpeaker = get_the_term_list($post->ID, 'speakers');
  $sermonSeries = get_the_term_list($post->ID, 'series');
 
- 	 $audio = "<audio  id='alabaster-sermoncontrol-audio'>";
+ 	 $audio = 				 "<audio  id='alabaster-sermoncontrol-audio'>";
  	 $audio = $audio . "<source id='alabaster-sermoncontrol-audioSource' src='" . $audioFile . "' type='audio/mp3'/>Your browser does not support the audio element. Please update browser.";
  	 $audio = $audio . "</audio>";
    $audio = $audio . "<div id='asm-player-body' class='clearfix'>";
@@ -27,14 +27,14 @@ if($style_options != '' ){
    $audio = $audio . "</div>";
    $audio = $audio . "<div id='asm-body-content'>";
    $audio = $audio . "<div id='asm-content-buttons'>";
-   $audio = $audio . "<a>";
+	 $audio = $audio . "<a id='asm-audio-download-link' href='". get_custom_field('sermon-audio-upload') ."'>";
    $audio = $audio . "<p id='asm-button-dwnld' class='asm-sec-color material-icons'>file_download</p>";
    $audio = $audio . "</a>";
    $audio = $audio . "<div id='asm-share-buttons'>";
-   $audio = $audio . "<a>";
+   $audio = $audio . "<a href='https://twitter.com/share?url=" . get_permalink() . "&text=Listen%20to%20" . get_the_title() . "' target='_blank'>";
    $audio = $audio . "<p id='asm-share-twitter' class='socicon-twitter'></p>";
    $audio = $audio . "</a>";
-   $audio = $audio . "<a>";
+   $audio = $audio . "<a href='https://www.facebook.com/sharer/sharer.php?u=". get_permalink() . "' target='_blank'>";
    $audio = $audio . "<p id='asm-share-facebook' class='socicon-facebook'></p>";
    $audio = $audio . "</a>";
    //Close share buttons
@@ -72,142 +72,6 @@ if($style_options != '' ){
 
 	?>
 <style>
-#asm-player-body{
-	background:#f5f5f5;
-	font-family: 'Source Sans Pro', sans-serif;
-	width: 75%;
-	margin: auto;
-	display: block;
-	position: relative;
-	min-height: 185px;
-	height: auto;
-}
-#asm-cover-image{
-	width: 25%;
-	background-size: cover;
-	background-position: center center;
-	display: block;
-	height: 185px;
-	float: left;
-	position: relative;
-}
-#asm-play-button{
-	color: #ffffff;
-	height: 60px;
-	width: 60px;
-	border-radius: 50%;
-	position: absolute;
-	right: -30px;
-	top: 50%;
-	transform: translateY(-50%);
-	-webkit-box-shadow: 0px 0px 9px 1px rgba(47,47,47,0.5);  /* Safari 3-4, iOS 4.0.2 - 4.2, Android 2.3+ */
-	-moz-box-shadow:    0px 0px 9px 1px rgba(47,47,47,0.5);  /* Firefox 3.5 - 3.6 */
-	box-shadow: 0px 0px 9px 1px rgba(47,47,47,0.5);
-	transition: all ease-in 0.5s;
-}
-#asm-play-button:hover{
-	box-shadow: none;
-	transition: all ease-in 0.5s;
-}
-
-#asm-play-button p{
-	position: relative;
-	margin: 0px;
-	top: 50%;
-	transform: translateY(-50%);
-	text-align: center;
-	display: block;
-	font-size: 2.5rem;
-}
-
-#asm-body-content{
-	float: left;
-	margin-left: 45px;
-	width: 68%;
-	position: relative;
-	height: 185px;
-}
-#asm-content-buttons{
-	position: absolute;
-	width: 100%;
-	top: 10px;
-}
-#asm-content-buttons p{
-	margin-top: 0px;
-}
-#asm-share-buttons{
-	float: right;
-}
-#asm-share-buttons a{
-	float: left;
-}
-#asm-button-dwnld{
-	transition: all ease-in 0.3s;
-}
-#asm-button-dwnld:hover{
-	transition: all ease-in 0.3s;
-	/*Replace with prime color*/
-	color: <?php echo $primary_style;?>;
-}
-
-#asm-share-twitter{
-	color:#00aced;
-	margin: 0px 5px;
-}
-#asm-share-facebook{
-	color:#3b5998;
-	margin: 0px 5px;
-}
-#asm-content-info{
-	width: 100%;
-	position: absolute;
-	top: 50%;
-	transform:translateY(-50%);
-}
-#asm-content-title{
-	margin: 0px;
-}
-#asm-content-captions{
-	margin-top: 0px;
-}
-#asm-content-scrub-controls{
-	line-height: 0.3;
-	width: 100%;
-	position: absolute;
-	bottom: 25px;
-}
-#asm-scrub-pos-container,
-#asm-scrub-length-container,
-#asm-content-scrub-bar{
-	float: left;
-	display: block;
-	box-sizing: content-box;
-}
-
-#asm-scrub-pos-container,
-#asm-scrub-length-container{
-	width: auto;
-	text-align: center;
-	cursor: arrow;
-}
-
-#asm-scrub-pos-container{
-	margin-right: 10px;
-}
-#asm-scrub-length-container{
-	margin-left: 10px;
-}
-
-
-#asm-content-scrub-bar,
-#asm-content-scrub-bar-current{
-	height: 7px;
-	display: block;
-	border-radius: 30px;
-}
-#asm-content-scrub-bar{
-	position: relative;
-}
 .asm-prime-bkg-color{
 	background:<?php echo $primary_style;?>;
 }
@@ -220,45 +84,4 @@ if($style_options != '' ){
 .asm-sec-color{
 	color:<?php echo $secondary_style;?>;
 }
-
-#asm-scrubbing-tooltip{
-	position: absolute;
-	width: auto;
-	height:25px;
-	color: #fff;
-	top:-30px;
-	text-align: center;
-	padding: 10px;
-	display:none;
-	left:-23.5px;
-	border-radius: 5px;
-}
-
-#asm-scrubbing-tooltip:after{
-	content:'';
-	 position: absolute;
-	 top: 100%;
-	 left: 0;
-	 right: 0;
-	 margin: 0 auto;
-	 width: 0;
-	 height: 0;
-	 border-top: solid 5px <?php echo $secondary_style;?>;
-	 border-left: solid 5px transparent;
-	 border-right: solid 5px transparent;
-}
-
-#asm-player-body .clearfix:after {
-	 visibility: hidden;
-	 display: block;
-	 font-size: 0;
-	 content: " ";
-	 clear: both;
-	 height: 0;
-	 }
-#asm-player-body .clearfix { display: inline-block; }
-/* start commented backslash hack \*/
-* html #asm-player-body .clearfix { height: 1%; }
-#asm-player-body .clearfix { display: block; }
-/* close commented backslash hack */
 </style>
