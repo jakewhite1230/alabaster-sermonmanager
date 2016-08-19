842ada88aac0d77c53a37ddff2970d1b51041d12
jakewhite1230 <jwhite@learn4life.org>
2016-08-19 15:34:07 -0700

    Updated Series Styling

--- inc/taxonomy-series.php
+++ inc/taxonomy-series.php
@@ -1,7 +1,10 @@
+<?php
+	$style_options = get_option('alabaster-sermonmanager-options');
+	if($style_options != '' ){
+		$primary_style = $style_options['alabaster-sermonmanager-primary-color'];
+		$secondary_style = $style_options['alabaster-sermonmanager-secondary-color'];
+	}
 
-
-		<?php 
-                  
 			//list terms in a given taxonomy
 			$taxonomy = 'series';
 			$series = get_terms($taxonomy);
@@ -12,21 +15,47 @@
 				echo '<p>There are no registered series.</p>';
 			} else {
 
-			foreach ($series as $singular) { 
+			foreach ($series as $singular) {
 				$t_id = $singular->term_id;
 				$term_meta = get_option( "series_$t_id" );
-				$seriesimage = esc_attr( $term_meta['image'] ) ? esc_attr( $term_meta['image'] ) : ''; 
+				$series_image = get_term_meta( $t_id, 'series-image', true );
 				?>
-				<div class="col-xs-12 col-sm-6 ">
-				<a href="<?php echo esc_attr(get_term_link($singular, $taxonomy)); ?>" title="<?php echo sprintf( __( "View all posts in %s" ), $singular->name ); ?>">
-				<img src="<?php echo $seriesimage;?>" alt="<?php echo $singular->name; ?>" style="width:100%;">
-				</a>
-					<a href="<?php echo esc_attr(get_term_link($singular, $taxonomy)); ?>" title="<?php echo sprintf( __( "View all posts in %s" ), $singular->name ); ?>"> <p style="margin:20px 0px;"><?php echo $singular->name; ?></p></a>
-				</div>
-			<?php
+				<div class="asm-tax-body clearfix">
+					<div class="asm-tax-profile-image">
+							<img src="<?php echo $series_image;?>" alt="<?php echo $singular->name; ?>" style="width:100%;">
+					</div>
+					<div class="asm-tax-content">
+							<h1 class="asm-tax-name"><?php echo $singular->name; ?></h1>
+						<?php
+							if($singular->description !=''){
+									?><p class="asm-tax-description"><?php echo $singular->description; ?></p><?php
+							}
+						 ?>
 
-				}
+						<?php	$args = array(
+										'post_type' => 'sermon',
+										'series' => $singular->name,
+										'posts_per_page' => 5
+								);
+								//  assigning variables to the loop
+									$wp_query = new WP_Query($args);
+									// starting loop
+									$sermonCount = $wp_query->post_count;
+									while ($wp_query->have_posts()) : $wp_query->the_post();?>
+										<p class="asm-tax-sermon-listings"><a class="asm-sermon-listing-link" href="<?php the_permalink();?>"><?php the_title();?></a></p>
+										<hr class="asm-tax-hr">
+									<?php endwhile;?>
+									<?php
+										if($sermonCount > 1){ ?>
+												<p style="margin:20px 0px;">
+													<a class='asm-listings-view-more' href="<?php echo esc_attr(get_term_link($singular, $taxonomy)); ?>" title="<?php echo sprintf( __( "View all posts in %s" ), $singular->name ); ?>">
+													View More
+												</a></p>
+									<?php	}?>
+										<?php wp_reset_query();?>
+					</div>
+				</div>
+			<?php }
 		}
 			?>
-
-
+<?php include('partials/asm-taxonomy-styles.php');?>
